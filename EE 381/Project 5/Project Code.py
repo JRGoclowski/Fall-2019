96312c7d559ee337cbf7d9c75c919ce9036b843c
Jonathan Goclowski <JRGoclowski@gmail.com>
2019-11-16 15:54:48 -0800

    Part 1 done

--- EE 381/Project 5/Project Code.py
+++ EE 381/Project 5/Project Code.py
@@ -41,9 +41,50 @@ popMean = mu
 popSTD = sigma
 sampleSize = n
 
-sample = np.random.normal(mu, sigma, n)
-print(sample)
-print("Xbar: " + str(XBar(sample)))
-print("Sigma Xbar: " + str(SigmaXBar(sigma, n)))
-print("S hat: " + str(sHat(sample, XBar(sample), n)))
-print("Sigma n: " + str(SigmaN(sample, XBar(sample), n)))
+########################################################
+### 1. Effect of sample size on confidence intervals ###
+########################################################
+sampData = np.zeros((n,1))
+sampXbar = np.zeros((n,1))
+sampSigXbar = np.zeros((n,1))
+sampSize = np.zeros((n,1))
+muLine = np.full((n,1), mu)
+
+for index in range(1,n + 1):
+    sampData = np.random.normal(mu, sigma, index)
+    sampXbar[index - 1] = XBar(sampData)
+    sampSigXbar[index - 1] = SigmaXBar(sigma, index)
+    sampSize[index - 1] = index
+
+PositiveCI95 = np.zeros((n,1))
+NegativeCI95 = np.zeros((n,1))
+
+for index in range(0,n):
+    PositiveCI95[index] = mu + 1.96*(sampSigXbar[index])
+    NegativeCI95[index] = mu - 1.96*(sampSigXbar[index])
+    plt.plot(sampSize[index], sampXbar[index], 'b', marker = 'x')
+
+plt.title("Sample Means and 95% confidence intervals", fontsize = 14, fontweight = 'bold')
+plt.xlabel("Sample Size", fontsize = 14)
+plt.ylabel("X_bar", fontsize = 14)
+plt.plot(sampSize, PositiveCI95, 'r',linestyle = '--')
+plt.plot(sampSize,muLine,'k')
+plt.plot(sampSize, NegativeCI95,'r',linestyle = '--')
+plt.show()
+
+PositiveCI99 = np.zeros((n,1))
+NegativeCI99 = np.zeros((n,1))
+
+for index in range(0,n):
+    PositiveCI99[index] = mu + 2.58*(sampSigXbar[index])
+    NegativeCI99[index] = mu - 2.58*(sampSigXbar[index])
+    plt.plot(sampSize[index], sampXbar[index], 'b', marker = 'x')
+
+plt.title("Sample Means and 99% confidence intervals", fontsize = 14, fontweight = 'bold')
+plt.xlabel("Sample Size", fontsize = 14)
+plt.ylabel("X_bar", fontsize = 14)
+plt.plot(sampSize, PositiveCI99, 'g',linestyle = '--')
+plt.plot(sampSize,muLine,'k')
+plt.plot(sampSize, NegativeCI99,'g',linestyle = '--')
+plt.show()
+    
