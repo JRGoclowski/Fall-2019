2e1f35b28b8158bbf771ef8971290815039ab1e0
Jonathan Goclowski <JRGoclowski@gmail.com>
2019-11-17 15:03:48 -0800

    more work

--- EE 381/Project 5/Project Code.py
+++ EE 381/Project 5/Project Code.py
@@ -23,13 +23,28 @@ def Normalize(xBar, mu, sigmaN):
 
 def CILarge(desiredInterval):
     pass
+
 def CISmall(desiredInterval):
     pass
+
 def muInterval(XBar, sHat, criticalValue, n):
     muInterval = np.zeros((2,1))
     muInterval[0] = XBar - (criticalValue*(sHat/m.sqrt(n)))
     muInterval[1] = XBar + (criticalValue*(sHat/m.sqrt(n)))
     return muInterval
+
+def successInCI(sample, repetitions, mu, sigma, CriticalValue):
+    sucessCount = 0
+    for index in range (0,repetitions):
+        data = np.random.normal (mu,sigma,sample)
+        xBar = XBar(data)
+        sampVar = sHat(data, xBar, sample)
+        muData = muInterval(xBar, sampVar, CriticalValue, sample)
+        if (mu > muData[0]) and (mu < muData[1]):
+            sucessCount += 1
+    return sucessCount
+        
+            
 
 N = 1200000
 mu = 45
@@ -40,7 +55,7 @@ totalBearings = N
 popMean = mu
 popSTD = sigma
 sampleSize = n
-
+"""
 ########################################################
 ### 1. Effect of sample size on confidence intervals ###
 ########################################################
@@ -87,4 +102,42 @@ plt.plot(sampSize, PositiveCI99, 'g',linestyle = '--')
 plt.plot(sampSize,muLine,'k')
 plt.plot(sampSize, NegativeCI99,'g',linestyle = '--')
 plt.show()
+"""
+################################################################
+### 2. Using the sample mean to estimate the population mean ###
+################################################################
+repeatCount = 10000
+
+### Trial Sample Sizes ###
+trialSampleSizes = [5, 40, 120]
+tSS = trialSampleSizes
+
+### Critical Values ###
+normalCriticalValues = [1.96, 2.58]
+nCV = normalCriticalValues
+
+tCriticalValues = [2.78, 4.60]
+tCV = tCriticalValues
+
+criticalValues = [nCV, tCV]
+cV = criticalValues
+
+confidenceTypes = ["95%", "99%"]
+cT = confidenceTypes
+
+distributionTypes = ["Normal", "Student's T"]
+dT = distributionTypes
+
+def CIRoutine(size, distribution, confidence):
+    sampleSize = tSS[size]
+    dist = cV[distribution]
+    critVal = dist[confidence]
+    sucesses = successInCI(sampleSize, repeatCount, mu, sigma, critVal)
+    print("Percent success with " + cT[confidence] + " confidence using " + dT[distribution] + "distribution: " + str((sucesses)/repeatCount * 100))
     
+    
+
+for sizeIndex in range (0,3):
+    for distributionIndex in range (0,2):
+        for confidenceIndex in range (0,2):
+            CIRoutine(sizeIndex, distributionIndex, confidenceIndex)
